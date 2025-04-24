c306843adf8d0d39c84b35bab52f224d21784690
Aleksander Szydlowski <aleksander.szydlowski@onet.pl>
2025-04-24 04:36:01 +0200

    feat, making the website accept manually inputted links (implementing this took approximately 2,5 hours of debugging)

--- Code/Backend/Classes/Program.cs
+++ Code/Backend/Classes/Program.cs
@@ -11,12 +11,26 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.Use(async (context, next) =>
+{
+    if (!context.Request.Path.Value.Contains('.') &&
+        !context.Request.Path.StartsWithSegments("/api"))
+    {
+        context.Response.ContentType = "text/html";
+        await context.Response.SendFileAsync(Path.Combine(app.Environment.WebRootPath, "index.html"));
+    }
+    else
+    {
+        await next();
+    }
+});
+
 app.MapGet("/api/info", () =>
 {
-    var time = DateTime.Now.ToString("HH:mm:ss");
-    var userId = new Random().Next(1000, 9999);
+    var time = DateTime.Now.ToString("HH:mm:ss"); 
+    var userId = new Random().Next(1000, 9999); 
 
-    return Results.Json(new { time, userId });
+    return Results.Json(new { time, userId }); 
 });
 
 app.Run();
