9e1f5d2156f8d2945ea3da2cb5209b75b15372b5
Aleksander Szydlowski <aleksander.szydlowski@onet.pl>
2025-04-24 05:00:46 +0200

    feat, displaying clothes listings from C#

--- Code/Backend/Classes/Program.cs
+++ Code/Backend/Classes/Program.cs
@@ -1,4 +1,6 @@
 
+using ReMarket.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -24,6 +26,16 @@ app.Use(async (context, next) =>
         await next();
     }
 });
+
+var clothesListings = new List<Listing>
+{
+    new Listing(0,"Test Jacked",100,new Category(0,"Clothes"),new Description("Example desc", "more useful text")),
+    new Listing(0,"Test Trousers",50,new Category(0,"Clothes"),new Description("Example desc", "more useful text lorem ipsum")),
+    new Listing(0,"Test Blazer",70,new Category(0,"Clothes"),new Description("Example desc", "more useful text")),
+    new Listing(0,"Test T-Shirt",20,new Category(0,"Clothes"),new Description("Example desc hello", "more useful text")),
+    new Listing(0,"Test Coat",600,new Category(0,"Clothes"),new Description("Example desc 123", "more useful text"))
+};
+app.MapGet("/api/clothes", () => Results.Json(clothesListings));
 
 app.MapGet("/api/info", () =>
 {
