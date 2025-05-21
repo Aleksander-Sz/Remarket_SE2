adb6b7233e7423ab534c1f60093858bbf1c6c1d0
Aleksander Szydlowski <aleksander.szydlowski@onet.pl>
2025-05-21 23:30:22 +0200

    fix, removing a secret from commit

--- Code/Backend/Classes/Services/AccountService.cs
+++ Code/Backend/Classes/Services/AccountService.cs
@@ -151,3 +151,10 @@ namespace ReMarket.Services
 
 
 }*/
+
+public class RegisterRequest
+{
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
